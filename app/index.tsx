16e7088e7b7a875e95dae494301430522cd1f6b5
Mateo Álvarez <mateo@netprogroup.com>
2025-02-13 17:07:35 -0500

    feat: add initial setup with expo and nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center h-full bg-gray-500">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
