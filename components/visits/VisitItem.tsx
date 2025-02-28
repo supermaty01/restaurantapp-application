619d9fed6e6dda103be92e283cf63473b982150d
Alejandro Alvarez Botero <aalvarezbo@unal.edu.co>
2025-02-27 23:40:45 -0500

    refactor: update list visits

--- components/visits/VisitItem.tsx
+++ components/visits/VisitItem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 interface VisitItemProps {
+  imageUrl: string | null;
   date: string;
   title: string;
   description: string;
@@ -10,6 +11,7 @@ interface VisitItemProps {
 }
 
 const VisitItem: React.FC<VisitItemProps> = ({
+  imageUrl,
   date,
   title,
   description,
@@ -21,7 +23,15 @@ const VisitItem: React.FC<VisitItemProps> = ({
       activeOpacity={0.8}
       className="bg-white p-4 rounded-xl mb-3 shadow-sm flex-row items-center justify-between"
     >
-      {/* Contenedor de texto */}
+      {imageUrl ? (
+        <Image
+          source={{ uri: imageUrl }}
+          className="w-14 h-14 rounded mr-3"
+          resizeMode="cover"
+        />
+      ) : (
+        <View className="w-14 h-14 rounded bg-gray-300 mr-3"/>
+      )}
       <View className="flex-1">
         <Text className="text-sm font-bold text-gray-800">
           {date} - <Text className="text-gray-900">{title}</Text>
@@ -29,8 +39,7 @@ const VisitItem: React.FC<VisitItemProps> = ({
         <Text className="text-xs text-gray-600">{description}</Text>
       </View>
 
-      {/* Icono de flecha */}
-      <Ionicons name="chevron-forward-outline" size={20} color="#6b6b6b" />
+      <Ionicons name="chevron-forward-outline" size={20} color="#6b6b6b"/>
     </TouchableOpacity>
   );
 };
