83cb0afa0d1cf6c7c0151a85f0124fd448cd7038
Mateo Álvarez <mateo@netprogroup.com>
2025-03-08 17:20:47 -0500

    feat: update config

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,13 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_lame_kinsey_walden.sql';
+import m0001 from './0001_left_lightspeed.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001
     }
   }
   
