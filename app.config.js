2c9f61a0e9e52eda4df8255f62560b7fdcd81c4c
Mateo Álvarez <mateo@netprogroup.com>
2026-03-09 18:31:05 -0500

    chore: version bump
    1.2.3

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "RestaurantApp",
     "slug": "restaurantapp",
-    "version": "1.2.2",
+    "version": "1.2.3",
     "orientation": "portrait",
     "icon": "./assets/burger-logo-fondo.png",
     "scheme": "myapp",
